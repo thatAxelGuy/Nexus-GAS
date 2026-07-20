bdbe3bcc10015b7a674a574856474135ce7eb707
Axel Woermann <53866066+thatAxelGuy@users.noreply.github.com>
2026-07-20 13:44:47 +0200

    Revert "Update visual studio settings"
    This reverts commit 88838fed1143254c4859420b0e8b2a3371ae63c0.

--- Source/NexusEditor.Target.cs
+++ Source/NexusEditor.Target.cs
@@ -8,7 +8,7 @@ public class NexusEditorTarget : TargetRules
 	public NexusEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V7;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
 
 		ExtraModuleNames.AddRange( new string[] { "Nexus" } );
 	}
